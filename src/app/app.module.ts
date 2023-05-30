deedda2e915de397345acc8bc9eece673682af10
8303244R <xavier.lancelle@sncf.fr>
2023-05-30 14:30:50 +0200

    [MS-FORM1] add ReactiveFormsModule in aa.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AddAudioTrackFormComponent } from './add-audio-track-form/add-audio-track-form.component';
@@ -10,7 +11,8 @@ import { AddAudioTrackFormComponent } from './add-audio-track-form/add-audio-tra
     AddAudioTrackFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
