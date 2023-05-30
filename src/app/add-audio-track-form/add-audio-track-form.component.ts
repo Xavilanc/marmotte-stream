7f27f358fb0db7348df9b22e271f92bc2a1771b2
8303244R <xavier.lancelle@sncf.fr>
2023-05-30 14:13:21 +0200

    [MS-FORM1] create add-audio-track-form component

---
+++ src/app/add-audio-track-form/add-audio-track-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-audio-track-form',
+  templateUrl: './add-audio-track-form.component.html',
+  styleUrls: ['./add-audio-track-form.component.css']
+})
+export class AddAudioTrackFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddAudioTrackFormComponent } from './add-audio-track-form/add-audio-track-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAudioTrackFormComponent
   ],
   imports: [
     BrowserModule
